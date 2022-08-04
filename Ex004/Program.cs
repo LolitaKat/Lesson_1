19a58f12bbb85f9d666636b75aba51cde8e61d5c
Лолита Катаева <kataeva.lla@gmail.com>
2022-08-04 22:03:43 +0300

    Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine( "Я так ждала тебя, Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
